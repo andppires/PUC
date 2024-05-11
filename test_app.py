7f0889dee8154197bdca56dcf7d8ccdb2d2f7042
Anderson Pires <79539843+7700453250@users.noreply.github.com>
2024-05-11 00:27:07 -0300

    Maquina Vitual e Testes unitarios

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 400
+    assert 'Olá, está preparado? Tudo está mudando! =)' in response.data.decode('utf-8')
