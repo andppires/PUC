d957a86d20bca0e4bb61ef99c5c2786d6353278d
Anderson Pires <79539843+7700453250@users.noreply.github.com>
2024-05-10 23:31:34 -0300

    projeto

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá, está preparado? Tudo está mudando!'
+    return 'Olá, está preparado? Tudo está mudando! =)'
 
 if __name__ == '__main__':
     app.run(debug=True)
