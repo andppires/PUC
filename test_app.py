08455caf3ce7c12c930fc704dac2aa3af1a35024
Anderson Pires <79539843+7700453250@users.noreply.github.com>
2024-05-11 00:46:12 -0300

    Teste

--- test_app.py
+++ test_app.py
@@ -9,4 +9,4 @@ def client():
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 400
-    assert 'Olá, está preparado? Tudo está mudando! =)' in response.data.decode('utf-8')
+    assert 'Olá, está preparado? Tudo está mudando! alterado =)' in response.data.decode('utf-8')
